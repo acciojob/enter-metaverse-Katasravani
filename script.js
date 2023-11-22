c25949545887be6e3a9183dd924d3882d9e5f8b3
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-22 12:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
-function clickme() {
-	let a=document.getElementById("status");
-	a.innerText="Entered Metaverse";
-}
+// function clickme() {
+// 	let a=document.getElementById("status");
+// 	a.type="h1";
+	
+// }
