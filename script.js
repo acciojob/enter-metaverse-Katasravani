5daeca40d6b38f65289b5d6e7634360505060a99
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-22 12:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //your JS code here. If required.
 // function clickme() {
+
 // 	let a=document.getElementById("status");
 // 	a.type="h1";
 	
 // }
+
+function click() {
+
+
+	const statusElement = document.getElementById('status');
+    const enterBtn = document.getElementById('enterBtn');
+	
+        // Create a new h1 element
+        const h1Element = document.createElement('h1');
+        
+        // Set its content
+        h1Element.textContent = 'Entered Metaverse';
+        
+        // Replace the p tag with the new h1 element
+        statusElement.parentNode.replaceChild(h1Element, statusElement);
+	
+
